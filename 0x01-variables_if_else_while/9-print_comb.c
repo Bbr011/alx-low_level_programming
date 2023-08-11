88c2142bc131a19d5a751efecc379d33529af25c
Bbr011 <khadraouih7@gmail.com>
2023-08-11 14:11:32 -0700

    8

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,12 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n < 9; n++)
+	for (n = 0; n < 10; n++)
 	{
 		if (n == 9)
 			putchar(n + '0');
 		else
-		{	
+		{
 			putchar(n + '0');
 			putchar(',');
 			putchar(' ');
