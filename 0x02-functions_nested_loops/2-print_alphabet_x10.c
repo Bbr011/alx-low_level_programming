23d6d70eb0c35e013e83c6e29036ab29ee3c7f5f
Bbr011 <khadraouih7@gmail.com>
2023-08-15 13:20:05 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char i;
+
+	for (i = 0 ; i < 10 ; i++)
+		_putchar(i);
+	_putchar('\n');
+}
