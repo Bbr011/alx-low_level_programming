481312b0eaa6b4befd05a085cd6e5eac1e6c389f
Bbr011 <khadraouih7@gmail.com>
2023-08-15 16:41:38 -0700

    101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int s, n;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if ((num % 3 == 0) || (n % 5 == 0))
+			s += n;
+	}
+	printf("%d\n", s);
+
+	return (0);
+}
