c8a943f7c994e38b415c7b143670c4b66ff3c897
Bbr011 <khadraouih7@gmail.com>
2023-08-17 13:18:06 -0700

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - function
+ * @n: parameter
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		int i;
+
+		for (i = 0 ; i <= n ; i++)
+			_putchar('_');
+		_putchar('\n');
+	} else
+		_putchar('\n');
+}
