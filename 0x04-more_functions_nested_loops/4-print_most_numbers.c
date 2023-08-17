07dadf8ff567a8c735453b7a29e4e4d86212a904
Bbr011 <khadraouih7@gmail.com>
2023-08-17 12:19:24 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int k;
+
+	for (k = 0 ; k < 10 ; k++)
+	{
+		if (k != 2 && k != 4)
+			_putchar(k + '0');
+	}
+}
