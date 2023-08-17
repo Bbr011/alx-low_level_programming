857d4620f4acafd0dc32f0fd274f920c519b6e95
Bbr011 <khadraouih7@gmail.com>
2023-08-17 13:43:37 -0700

    7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,12 +12,14 @@ void print_diagonal(int n)
 		_putchar('\n');
 	else
 	{
-		int i;
+		int i, j;
 
-		for (i = 1 ; i <= n ; i++)
-			_putchar(' ');
-		_putchar('\\');
-		_putchar('\n');
-
+		for (i = 0 ; i < n ; i++)
+		{
+			for (j = 0 ; j <= i ; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
 	}
 }
