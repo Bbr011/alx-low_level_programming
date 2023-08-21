608a4a0358c4a22e70235abbfd21c47837e2284d
Bbr011 <khadraouih7@gmail.com>
2023-08-21 13:06:02 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - function
+ * @s: parameter
+ * Return: s
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int j = 0;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+		j++;
+	return (j);
+
+}
