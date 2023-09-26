d482bc82a014e16267dd32022c980f3f2b1d5040
Bbr011 <khadraouih7@gmail.com>
2023-09-26 10:31:07 -0700

    0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - function
+ * @h: parameter
+ * Return: c
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t c = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		c++;
+		h = h->next;
+	}
+	return (c);
+}
