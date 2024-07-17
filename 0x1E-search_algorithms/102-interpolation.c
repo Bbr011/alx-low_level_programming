9e0feda27b95bffdd8b73c6bbc28ac418817a9b3
Bbr011 <kahdraouih7@gmail.com>
2024-07-17 15:39:26 -0700

    Interpolation search

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,63 +1,35 @@
 #include "search_algos.h"
 
 /**
- * exponential_search - performs exponential search
+ * interpolation_search - performs jump search
  * @array: the integer array
  * @size: its size
  * @value: value to search for
  * Return: the index found or -1
  */
-int exponential_search(int *array, size_t size, int value)
+int interpolation_search(int *array, size_t size, int value)
 {
-	size_t i = 1, newsize = 0;
-	int ret;
+	size_t pos = 0, low = 0, high = size - 1;
 
 	if (!array || !size)
 		return (-1);
 
-	while (i < size && array[i] < value)
+	while (1)
 	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		i <<= 1;
-	}
-	newsize = (i >= size ? size : i + 1) - (i >> 1);
-	i >>= 1;
-	printf("Value found between indexes [%lu] and [%lu]\n",
-			i, i << 1 >= size ? size - 1 : i << 1);
-	ret = binary_search(array + i, newsize, value);
-	return (ret == -1 ? ret : ret + (int)i);
-}
-
-/**
- * binary_search - performs binary search
- * @array: the integer array
- * @size: its size
- * @value: value to search for
- * Return: the index found or -1
- */
-int binary_search(int *array, size_t size, int value)
-{
-	size_t i = 0;
-	int *a = array;
-
-	if (!array)
-		return (-1);
-
-	while (size)
-	{
-		for (i = 0, printf("Searching in array: "); i < size; i++)
-			printf("%d%s", a[i], i + 1 == size ? "\n" : ", ");
-
-		i = (size - 1) / 2;
-		if (a[i] == value)
-			return ((a - array) + i);
-		else if (a[i] > value)
-			size = i;
-		else
+		pos = low + (((double)(high - low) /
+			  (array[high] - array[low])) * (value - array[low]));
+		if (pos >= size)
 		{
-			a += (i + 1);
-			size -= (i + 1);
+			printf("Value checked array[%lu] is out of range\n", pos);
+			break;
 		}
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+		if (array[pos] == value)
+			return (pos);
+		else if (array[pos] > value)
+			high = pos - 1;
+		else
+			low = pos + 1;
 	}
 	return (-1);
 }
